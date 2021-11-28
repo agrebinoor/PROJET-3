a5b67a9681fa4ce8247d3b27a7940e241e77b0ae
Agrebi Noor <agrebinoor@gmail.com>
2021-11-28 21:24:22 +0100

    Update ethereum.ts

--- src/services/ethereum.ts
+++ src/services/ethereum.ts
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-import ContractInterface from '../build/contracts/BuildCollective.json'
+import ContractInterface from '../../build/contracts/BuildCollective.json'
 
 const win: any = window
 
